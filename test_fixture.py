42cf11962890cc8e3dff63503bbdff55a332e623
Svetlana Sadikova <s.sadikova@sigma.ru>
2021-12-04 15:47:58 +0300

    add fixture file

---
+++ test_fixture.py
@@ -0,0 +1,43 @@
+import os
+import random
+
+import pytest
+
+folder = 'new_folder'
+
+
+# @pytest.fixture(scope="function")
+# def create_folder():
+#     if not os.path.exists(folder):
+#         os.makedirs(folder)
+#
+#
+# def test_created(create_folder):
+#     assert os.path.exists(folder), 'folder not exist'
+#
+#
+# def test_created_2(create_folder):
+#     assert os.path.exists(folder), 'folder not exist'
+
+
+@pytest.fixture()
+def get_rand_num():
+    num = random.randint(1, 100)
+    print(num)
+    return num
+
+
+def test_rand_number(get_rand_num):
+    assert 0 < get_rand_num <= 100
+
+
+def test_rand_number_2(get_rand_num):
+    assert 0 < get_rand_num <= 100
+
+
+class RandNumber:
+    def test_rand_number_2(self, get_rand_num):
+        assert 0 < get_rand_num <= 100
+
+    def test_rand_number_4(self, get_rand_num):
+        assert 0 < get_rand_num <= 100
